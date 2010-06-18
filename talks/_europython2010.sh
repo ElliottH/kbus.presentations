19433e226c00ff60a498a2acf4b33b5d6dab1815
tibs@tibsnjoan.co.uk <none@none>
2010-06-18 14:31:02 +0000

    rst2pdf added to shell script
    --HG--
    extra : convert_revision : svn%3A53f7ac7a-121e-11de-a074-998af0e899fe/trunk%40368

--- talks/_europython2010.sh
+++ talks/_europython2010.sh
@@ -3,3 +3,7 @@ echo Reading europython2010.txt, preparing _europython2010.tmp
 ./minicog.py europython2010.txt _europython2010.tmp
 echo Reading _europython.tmp, preparing _europython.html
 rst2html _europython2010.tmp > _europython2010.html
+echo Reading _europython.tmp, preparing _europython.pdf
+rst2pdf _europython.tmp -o _europython.pdf \
+                --fit-literal-mode=overflow \
+                -v
