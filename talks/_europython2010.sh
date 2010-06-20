f4075be7f5d0e9506f51b2e6e80daf06cd531972
tibs@tibsnjoan.co.uk <none@none>
2010-06-20 17:51:42 +0000

    Sort out the goldfish bowl images a bit, in the europython talk
    --HG--
    extra : convert_revision : svn%3A53f7ac7a-121e-11de-a074-998af0e899fe/trunk%40369

--- talks/_europython2010.sh
+++ talks/_europython2010.sh
@@ -3,7 +3,7 @@ echo Reading europython2010.txt, preparing _europython2010.tmp
 ./minicog.py europython2010.txt _europython2010.tmp
 echo Reading _europython.tmp, preparing _europython.html
 rst2html _europython2010.tmp > _europython2010.html
-echo Reading _europython.tmp, preparing _europython.pdf
-rst2pdf _europython.tmp -o _europython.pdf \
+echo Reading _europython2010.tmp, preparing _europython2010.pdf
+rst2pdf _europython2010.tmp -o _europython2010.pdf \
                 --fit-literal-mode=overflow \
                 -v
