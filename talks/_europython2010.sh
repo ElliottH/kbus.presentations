03c78eb4dc638bb8d81fe3354f828276364bde5c
tibs@tibsnjoan.co.uk <none@none>
2010-06-28 15:54:56 +0000

    On mu work machine, the EuroPython paper script will install KBUS for me
    --HG--
    extra : convert_revision : svn%3A53f7ac7a-121e-11de-a074-998af0e899fe/trunk%40384

--- talks/_europython2010.sh
+++ talks/_europython2010.sh
@@ -1,4 +1,14 @@
 #! /bin/sh
+machine=`uname -n`
+echo Machine $machine
+if [ x"$machine" == x"spoon" ]
+then
+  echo "Installing KBUS"
+  pushd ~/sw/kbus/kbus
+  sudo insmod kbus.ko
+  popd
+fi
+
 echo Reading europython2010.txt, preparing _europython2010.tmp
 ./minicog.py europython2010.txt _europython2010.tmp
 echo Reading _europython.tmp, preparing _europython.html
@@ -6,3 +16,9 @@ rst2html _europython2010.tmp > _europython2010.html
 echo Reading _europython2010.tmp, preparing _europython2010.pdf
 rst2pdf _europython2010.tmp -o _europython2010.pdf \
                 --fit-literal-mode=overflow
+
+if [ x"$machine" == x"spoon" ]
+then
+  echo "Uninstalling KBUS"
+  sudo rmmod kbus
+fi
